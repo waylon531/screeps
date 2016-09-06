e9fd0320113f557220bb59ab2b0991ab883083cf
waylon531 <waylon531@firechicken.net>
2016-09-06 12:47:53 -0700

    Added upgrader type

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 var worker = require('worker');
+var upgrader= require('upgrader');
 var miner = require('miner');
 var util = require('util');
 var workers = 0;
 var miners = 0;
+var upgraders = 0;
 for(var i in Game.creeps) {
     if (Game.creeps[i].memory.type == 'miner') {
         miners += 1;
@@ -10,11 +12,16 @@ for(var i in Game.creeps) {
     } else if (Game.creeps[i].memory.type == 'worker') {
         workers +=1;
         worker.run(Game.creeps[i]);
+    } else if (Game.creeps[i].memory.type == 'upgrader') {
+        upgraders +=1;
+        upgrader.run(Game.creeps[i]);
     }
 }
 if (Game.spawns['Nice'].canCreateCreep([CARRY,WORK,MOVE]) == 0) {
     if (miners < 10) {
         miner.spawn(Game.spawns['Nice']);
+    } else if (upgraders < 2) {
+        upgrader.spawn(Game.spawns['Nice']);
     } else if (workers < 8) {
         worker.spawn(Game.spawns['Nice']);
     }
