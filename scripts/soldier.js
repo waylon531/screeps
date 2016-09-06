b245d48ecf52eaf50fdcc46ea3938220eb28218f
waylon531 <waylon531@firechicken.net>
2016-09-06 15:14:23 -0700

    Fix?

--- scripts/soldier.js
+++ scripts/soldier.js
@@ -3,7 +3,7 @@ module.exports = {
     run(creep) {
         var error = 0;
         if(!creep.memory.target) { 
-            if (creep.room.name == Game.flags["Flag1"].room.name) {
+            if (creep.pos.roomName == Game.flags["Flag1"].pos.roomName) {
                 var creepTarget = Game.flags["Flag1"].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                 var structureTarget = Game.flags["Flag1"].pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
             //var creepDefenseTarget = Game.spawns["Nice"].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
