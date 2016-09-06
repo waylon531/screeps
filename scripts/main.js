f1f4affb3332a992d139d334aa188fe404d7ec79
waylon531 <waylon531@firechicken.net>
2016-09-06 01:19:16 -0700

    Initial autospawn logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,22 @@
 var worker = require('worker');
 var miner = require('miner');
+var util = require('util');
+var workers = 0;
+var miners = 0;
 for(var i in Game.creeps) {
     if (Game.creeps[i].memory.type == 'miner') {
+        miners += 1;
         miner.run(Game.creeps[i]);
     } else if (Game.creeps[i].memory.type == 'worker') {
+        workers +=1;
         worker.run(Game.creeps[i]);
     }
 }
+if (Game.spawns['Nice'].canCreateCreep([CARRY,WORK,MOVE])) {
+    if (miners < 6) {
+        miner.spawn(Game.spawns['Nice']);
+    } else if (workers < 6) {
+        worker.spawn(Game.spawns['Nice']);
+    }
+}
+
