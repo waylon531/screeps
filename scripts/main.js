270acdfb8e27609533c88755bbf79a26573af729
waylon531 <waylon531@firechicken.net>
2016-09-06 01:20:18 -0700

    Fix

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ for(var i in Game.creeps) {
         worker.run(Game.creeps[i]);
     }
 }
-if (Game.spawns['Nice'].canCreateCreep([CARRY,WORK,MOVE])) {
+if (Game.spawns['Nice'].canCreateCreep([CARRY,WORK,MOVE]) == 0) {
     if (miners < 6) {
         miner.spawn(Game.spawns['Nice']);
     } else if (workers < 6) {
