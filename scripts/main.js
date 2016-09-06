49ee81f30617bb60de663d8cfb539e74bd66a546
waylon531 <waylon531@firechicken.net>
2016-09-05 21:51:38 -0700

    Removed builder module

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,4 @@
 var worker = require('worker');
-var builder = require('builder');
 for(var i in Game.creeps) {
-    if (Game.creeps[i].name == 'BobTheBuilder') {
-        worker.run(Game.creeps[i]);
-    } else {
-        worker.run(Game.creeps[i]);
-    }
+    worker.run(Game.creeps[i]);
 }
