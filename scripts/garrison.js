029639c0ab9d1dd212d5f882758f2305b4792479
waylon531 <waylon531@firechicken.net>
2016-09-06 15:25:19 -0700

    Added garrison troops

---
+++ scripts/garrison.js
@@ -0,0 +1,29 @@
+var util = require('util');
+module.exports = {
+    run(creep) {
+        var error = 0;
+        if(!creep.memory.target) { 
+                var creepTarget = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                var structureTarget = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+            //var creepDefenseTarget = Game.spawns["Nice"].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            //if (creepDefenseTarget) {
+            //    creep.memory.target = creepDefenseTarget.id;
+                if (creepTarget) {
+                    creep.memory.target = creepTarget.id;
+                }else if (structureTarget) {
+                    creep.memory.target = structureTarget.id;
+                }
+        } else {
+            error = creep.attack(Game.getObjectById(creep.memory.target));
+            if(error == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.getObjectById(creep.memory.target));
+            }
+        }
+        if (error == ERR_INVALID_TARGET) {
+            creep.memory.target = false;
+        }
+    },
+    spawn(spawner) {
+        return spawner.createCreep([ATTACK,ATTACK,MOVE,MOVE],null,{type: 'garrison'});
+    }
+};
--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,13 @@ var worker = require('worker');
 var upgrader= require('upgrader');
 var miner = require('miner');
 var soldier = require('soldier');
+var garrison = require('garrison');
 var util = require('util');
 var workers = 0;
 var miners = 0;
 var upgraders = 0;
 var soldiers = 0;
+var garrisons = 0;
 for(var i in Game.creeps) {
     if (Game.creeps[i].memory.type == 'miner') {
         miners += 1;
@@ -17,6 +19,9 @@ for(var i in Game.creeps) {
     } else if (Game.creeps[i].memory.type == 'soldier') {
         soldiers +=1;
         soldier.run(Game.creeps[i]);
+    } else if (Game.creeps[i].memory.type == 'garrison') {
+        garrisons +=1;
+        garrison.run(Game.creeps[i]);
     } else if (Game.creeps[i].memory.type == 'upgrader') {
         upgraders +=1;
         upgrader.run(Game.creeps[i]);
@@ -29,7 +34,9 @@ if (Game.spawns['Nice'].canCreateCreep([CARRY,WORK,MOVE]) == 0) {
         upgrader.spawn(Game.spawns['Nice']);
     } else if (workers < 6) {
         worker.spawn(Game.spawns['Nice']);
-    } else if (soldiers < 4) {
+    } else if (garrisons < 4) {
+        garrison.spawn(Game.spawns['Nice']);
+    } else if (soldiers < 10) {
         soldier.spawn(Game.spawns['Nice']);
     }
 }
