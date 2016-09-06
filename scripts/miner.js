4aa92bd5d6f46909cceea7e08396143e4702c7db
waylon531 <waylon531@firechicken.net>
2016-09-06 00:32:17 -0700

    Changed order of miner logic

--- scripts/miner.js
+++ scripts/miner.js
@@ -2,21 +2,6 @@ var util = require('util');
 module.exports = {
     run(creep) {
         var total = _.sum(creep.carry);
-        if (creep.memory.transfer == false) {
-            //Mine
-            var target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
-            if(creep.memory.target) {
-                if(creep.harvest(Game.getObjectById(target)) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(Game.getObjectById(target));
-                }
-            }
-        } else {
-            //Transfer resources
-            var target = util.findNearestContainer(creep);
-            if(creep.transfer(target,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
-            }
-        }
         if(total >=50) {
             //Clear target
             Memory[target.id] -= 1;
@@ -38,6 +23,21 @@ module.exports = {
                 Memory[id] +=1;
             }
             creep.memory.transfer = false;
+        }
+        if (creep.memory.transfer == false) {
+            //Mine
+            var target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+            if(creep.memory.target) {
+                if(creep.harvest(Game.getObjectById(target)) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.getObjectById(target));
+                }
+            }
+        } else {
+            //Transfer resources
+            var target = util.findNearestContainer(creep);
+            if(creep.transfer(target,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
         }
     },
     spawn(spawner) {
