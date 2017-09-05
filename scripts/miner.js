1fdf39b7aeaa2bb14c156c300a82b9b2e3a8ee69
Waylon Cude <waylon531@firechicken.net>
2017-09-05 13:00:36 -0700

    Increased size of miner

--- scripts/miner.js
+++ scripts/miner.js
@@ -50,6 +50,6 @@ module.exports = {
         }
     },
     spawn(spawner, num) {
-        return spawner.createCreep([CARRY,CARRY,CARRY,WORK,WORK,WORK,WORK,WORK,MOVE,MOVE],null,{type: 'miner', transfer: false, source: num});
+        return spawner.createCreep([CARRY,CARRY,CARRY,CARRY,WORK,WORK,WORK,WORK,WORK,WORK,MOVE,MOVE],null,{type: 'miner', transfer: false, source: num});
     }
 };
