4f114d826d8bcdf16410c656ec52c7ee41c59f71
Waylon Cude <waylon531@firechicken.net>
2017-09-05 09:19:53 -0700

    Removed extra brace?

--- scripts/thief.js
+++ scripts/thief.js
@@ -15,66 +15,63 @@ module.exports = {
                         //Find buildings that we can steal energy from
                         return ( 
                             ( ( ( object.structureType == STRUCTURE_SPAWN || object.structureType == STRUCTURE_EXTENSION ) && object.energy ) ||
-                              ( ( object.structureType == STRUCTURE_CONTAINER || object.structureType == STRUCTURE_STORAGE ) && object.store[RESOURCE_ENERGY]) ) &&
+                                ( ( object.structureType == STRUCTURE_CONTAINER || object.structureType == STRUCTURE_STORAGE ) && object.store[RESOURCE_ENERGY]) ) &&
                             ( ! util.onWhitelist(object.owner) ));
-                            }
-                            });
-                        //var creepDefenseTarget = Game.spawns["Nice"].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-                        //if (creepDefenseTarget) {
-                        //    creep.memory.target = creepDefenseTarget.id;
-                        //Mine
-                        if (target) {
-                            creep.memory.target = creepTarget.id;
-                        } else {
-                            //No targets
-                            creep.moveTo(flag);
-                        }
-                        } else {
-                            creep.memory.attack = false;
-                            creep.moveTo(flag);
-                        }
-            } else {
-                let error = creep.withdraw(Game.getObjectById(creep.memory.target),RESOURCE_ENERGY);
-                if(error == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(Game.getObjectById(creep.memory.target));
-                } else if (error == ERR_NOT_ENOUGH_RESOURCES || error == ERR_INVALID_TARGET) {
-                    creep.memory.target = false;
-                }
-            }
-            if (_.sum(creep.carry) == creep.carryCapacity) {
-                let flag = Games.flags["Home"];
-                if (creep.pos.roomName == flag.pos.roomName && !creep.memory.attack) {
-                    //Spend another turn moving towards the flag
-                    //This should prevent creeps from flip-flopping across the border
-                    creep.moveTo(flag);
-                    creep.memory.attack = true;
-                } else if (creep.pos.roomName == flag.pos.roomName) {
-                    //Transfer resources
-                    if (!creep.memory.transferTarget) {
-                        var extensionTarget = util.findNearestEmptyExtension(creep);
-                        let spawnTarget = util.findNearestSpawn(creep);
-                        if (extensionTarget) {
-                            //Send energy to extensions first as they don't regen
-                            creep.memory.transferTarget = extensionTarget.id;
-                        } else if(spawnTarget.energy < spawnTarget.energyCapacity) {
-                            //If spawn is not full
-                            creep.memory.transferTarget = spawnTarget.id;
-                        }
                     }
-                    let error = creep.transfer(Game.getObjectById(creep.memory.transferTarget),RESOURCE_ENERGY);
-                    if(error == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(Game.getObjectById(creep.memory.transferTarget));
-                    } else if (error == ERR_FULL || error == ERR_INVALID_TARGET) {
-                        creep.memory.transferTarget = false;
-                    }
+                });
+                //var creepDefenseTarget = Game.spawns["Nice"].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                //if (creepDefenseTarget) {
+                //    creep.memory.target = creepDefenseTarget.id;
+                //Mine
+                if (target) {
+                    creep.memory.target = creepTarget.id;
                 } else {
-                    creep.memory.attack = false;
+                    //No targets
                     creep.moveTo(flag);
                 }
+            } else {
+                creep.memory.attack = false;
+                creep.moveTo(flag);
             }
-
+        } else {
+            let error = creep.withdraw(Game.getObjectById(creep.memory.target),RESOURCE_ENERGY);
+            if(error == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.getObjectById(creep.memory.target));
+            } else if (error == ERR_NOT_ENOUGH_RESOURCES || error == ERR_INVALID_TARGET) {
+                creep.memory.target = false;
+            }
         }
-
+        if (_.sum(creep.carry) == creep.carryCapacity) {
+            let flag = Games.flags["Home"];
+            if (creep.pos.roomName == flag.pos.roomName && !creep.memory.attack) {
+                //Spend another turn moving towards the flag
+                //This should prevent creeps from flip-flopping across the border
+                creep.moveTo(flag);
+                creep.memory.attack = true;
+            } else if (creep.pos.roomName == flag.pos.roomName) {
+                //Transfer resources
+                if (!creep.memory.transferTarget) {
+                    var extensionTarget = util.findNearestEmptyExtension(creep);
+                    let spawnTarget = util.findNearestSpawn(creep);
+                    if (extensionTarget) {
+                        //Send energy to extensions first as they don't regen
+                        creep.memory.transferTarget = extensionTarget.id;
+                    } else if(spawnTarget.energy < spawnTarget.energyCapacity) {
+                        //If spawn is not full
+                        creep.memory.transferTarget = spawnTarget.id;
+                    }
+                }
+                let error = creep.transfer(Game.getObjectById(creep.memory.transferTarget),RESOURCE_ENERGY);
+                if(error == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.getObjectById(creep.memory.transferTarget));
+                } else if (error == ERR_FULL || error == ERR_INVALID_TARGET) {
+                    creep.memory.transferTarget = false;
+                }
+            } else {
+                creep.memory.attack = false;
+                creep.moveTo(flag);
+            }
+        }
     },
     spawn(spawner,flagName) {
         return spawner.createCreep([CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE],null,{type: 'thief', attack: false, flagName });
